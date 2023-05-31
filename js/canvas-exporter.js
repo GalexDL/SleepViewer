7d9386d9f786abd93bd3b60c6809cf4628863cb5
Galex <43304388+GalexDL@users.noreply.github.com>
2023-05-31 14:25:13 -0400

    Update canvas-exporter.js

--- js/canvas-exporter.js
+++ js/canvas-exporter.js
@@ -1,4 +1,8 @@
 // Main
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 function exportAnimation(FPS = 60) {
   let exportCanvas = document.createElement("canvas");
   exportCanvas.id = "export-canvas";
@@ -25,37 +29,63 @@ function exportAnimation(FPS = 60) {
       appExport.stage.addChild(exportChar);
 
       // Export Section
-      let videoStream = exportCanvas.captureStream(FPS); //default to 60
-      let mediaRecorder = new MediaRecorder(videoStream);
+      let canvasStream = exportCanvas.captureStream(FPS); // default to 60
+      let mediaRecorder = new MediaRecorder(canvasStream);
+      let gifChunks = [];
 
-      let chunks = [];
       mediaRecorder.ondataavailable = function (e) {
-        chunks.push(e.data);
+        gifChunks.push(e.data);
       };
 
       mediaRecorder.onstop = function (e) {
-        let blob = new Blob(chunks, { type: option.exportType.value });
-        chunks = [];
-        let videoURL = URL.createObjectURL(blob);
-        exportVideo.src = videoURL;
-      };
-      mediaRecorder.ondataavailable = function (e) {
-        chunks.push(e.data);
+        let exportType = option.exportType.value;
+
+        if (exportType === "image/gif") {
+          let gif = new GIF({
+            workers: 2,
+            quality: 10,
+          });
+
+          gif.on("finished", function (blob) {
+            let gifURL = URL.createObjectURL(blob);
+            exportVideo.src = gifURL;
+          });
+
+          let frames = [];
+          for (let i = 0; i < gifChunks.length; i++) {
+            let img = new Image();
+            img.src = URL.createObjectURL(gifChunks[i]);
+            frames.push(img);
+          }
+
+          frames.forEach(function (frame) {
+            gif.addFrame(frame, { delay: 1000 / FPS });
+          });
+
+          gif.render();
+        } else {
+          let blob = new Blob(gifChunks, { type: exportType });
+          let videoURL = URL.createObjectURL(blob);
+          exportVideo.src = videoURL;
+        }
+
+        gifChunks = [];
       };
 
       // Get Animation Length
       let animLength = 0;
-      for (var i in char.spineData.animations) {
-        if (char.spineData.animations[i].name == option.animations.value) {
+      for (let i in char.spineData.animations) {
+        if (char.spineData.animations[i].name === option.animations.value) {
           animLength = char.spineData.animations[i].duration;
           break;
         }
       }
 
-      //Modal Popup
+      // Modal Popup
       document.getElementById("rendering").style.display = "block";
       document.getElementById("complete").style.display = "none";
       UIkit.modal(document.getElementById("modal-exporter")).show();
+
       // Progressbar
       document.getElementById("export-progress").value = 0;
       let progress = setInterval(function () {
@@ -66,19 +96,20 @@ function exportAnimation(FPS = 60) {
       mediaRecorder.start();
       setTimeout(function () {
         mediaRecorder.stop();
-        //Free Resources
+        // Free Resources
         appExport.stage.children.pop();
         appExport.loader.resources = {};
         exportCanvas.remove();
         clearInterval(progress);
 
-        //Update modal
+        // Update modal
         document.getElementById("rendering").style.display = "none";
         document.getElementById("complete").style.display = "block";
         document.getElementById("result").appendChild(exportVideo);
       }, animLength * 1000);
     });
 }
+
 
 // char.state.setAnimation(0, "Idle_01", false);
 // mediaRecorder.start();
